61601da9a247970df63d49ea52999e69cd4214de
Drew Stone <drewstone329@gmail.com>
2020-10-15 13:14:12 -0400

    Initial commit

---
+++ pallets/worker/src/lib.rs
@@ -0,0 +1,130 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use frame_system::{
+	self as system,
+	offchain::{
+		AppCrypto, CreateSignedTransaction,
+	}
+};
+use frame_support::{
+	debug, decl_module, decl_storage, decl_event,
+	traits::Get,
+};
+use sp_core::crypto::KeyTypeId;
+use sp_runtime::{
+	transaction_validity::{
+		ValidTransaction, TransactionValidity, TransactionSource,
+		TransactionPriority,
+	},
+};
+
+/// This pallet's configuration trait
+pub trait Trait: CreateSignedTransaction<Call<Self>> {
+	/// The identifier type for an offchain worker.
+	type AuthorityId: AppCrypto<Self::Public, Self::Signature>;
+
+	/// The overarching event type.
+	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+	/// The overarching dispatch call type.
+	type Call: From<Call<Self>>;
+
+	// Configuration parameters
+
+	/// A grace period after we send transaction.
+	///
+	/// To avoid sending too many transactions, we only attempt to send one
+	/// every `GRACE_PERIOD` blocks. We use Local Storage to coordinate
+	/// sending between distinct runs of this offchain worker.
+	type GracePeriod: Get<Self::BlockNumber>;
+
+	/// Number of blocks of cooldown after unsigned transaction is included.
+	///
+	/// This ensures that we only accept unsigned transactions once, every `UnsignedInterval` blocks.
+	type UnsignedInterval: Get<Self::BlockNumber>;
+
+	/// A configuration for base priority of unsigned transactions.
+	///
+	/// This is exposed so that it can be tuned for particular runtime, when
+	/// multiple pallets send unsigned transactions.
+	type UnsignedPriority: Get<TransactionPriority>;
+}
+
+decl_storage! {
+	trait Store for Module<T: Trait> as WorkerModule {}
+}
+
+decl_event!(
+	pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
+		NewHeader(u32, AccountId),
+	}
+);
+
+decl_module! {
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+		// // Errors must be initialized if they are used by the pallet.
+		// type Error = Error<T>;
+
+		// Events must be initialized if they are used by the pallet.
+		fn deposit_event() = default;
+
+		/// Offchain Worker entry point.
+		///
+		/// By implementing `fn offchain_worker` within `decl_module!` you declare a new offchain
+		/// worker.
+		/// This function will be called when the node is fully synced and a new best block is
+		/// succesfuly imported.
+		/// Note that it's not guaranteed for offchain workers to run on EVERY block, there might
+		/// be cases where some blocks are skipped, or for some the worker runs twice (re-orgs),
+		/// so the code should be able to handle that.
+		/// You can use `Local Storage` API to coordinate runs of the worker.
+		fn offchain_worker(block_number: T::BlockNumber) {
+			// It's a good idea to add logs to your offchain workers.
+			// Using the `frame_support::debug` module you have access to the same API exposed by
+			// the `log` crate.
+			// Note that having logs compiled to WASM may cause the size of the blob to increase
+			// significantly. You can use `RuntimeDebug` custom derive to hide details of the types
+			// in WASM or use `debug::native` namespace to produce logs only when the worker is
+			// running natively.
+			debug::native::info!("Hello World from offchain workers!");
+
+			// Since off-chain workers are just part of the runtime code, they have direct access
+			// to the storage and other included pallets.
+			//
+			// We can easily import `frame_system` and retrieve a block hash of the parent block.
+			let parent_hash = <system::Module<T>>::block_hash(block_number - 1.into());
+			debug::debug!("Current block: {:?} (parent hash: {:?})", block_number, parent_hash);
+		}
+	}
+}
+
+#[allow(deprecated)] // ValidateUnsigned
+impl<T: Trait> frame_support::unsigned::ValidateUnsigned for Module<T> {
+	type Call = Call<T>;
+
+	/// Validate unsigned call to this module.
+	///
+	/// By default unsigned transactions are disallowed, but implementing the validator
+	/// here we make sure that some particular calls (the ones produced by offchain worker)
+	/// are being whitelisted and marked as valid.
+	fn validate_unsigned(
+		_source: TransactionSource,
+		_call: &Self::Call,
+	) -> TransactionValidity {
+		ValidTransaction::with_tag_prefix("ExampleOffchainWorker")
+		// We set base priority to 2**20 and hope it's included before any other
+		// transactions in the pool. Next we tweak the priority depending on how much
+		// it differs from the current average. (the more it differs the more priority it
+		// has).
+		.priority(T::UnsignedPriority::get())
+		// The transaction is only valid for next 5 blocks. After that it's
+		// going to be revalidated by the pool.
+		.longevity(5)
+		// It's fine to propagate that transaction to other peers, which means it can be
+		// created even by nodes that don't produce blocks.
+		// Note that sometimes it's better to keep it for yourself (if you are the block
+		// producer), since for instance in some schemes others may copy your solution and
+		// claim a reward.
+		.propagate(true)
+		.build()
+	}
+}
